e4e6021c5f27f79ac0d8dd49d525b56fd0c7b666
Anik Shakya <aniklinkin@gmail.com>
2024-11-22 15:55:50 +0545

    [Added:: Bottom Nav]

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MaterialApp(
+    theme: ThemeData.dark(), // Optional: Dark theme for better visual
+    home: const FloatingBottomNavBar(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FloatingBottomNavBar extends StatefulWidget {
+  const FloatingBottomNavBar({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _FloatingBottomNavBarState createState() => _FloatingBottomNavBarState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FloatingBottomNavBarState extends State<FloatingBottomNavBar> {
+  int _selectedIndex = 0;
+  final ScrollController _scrollController = ScrollController();
+  bool _isNavVisible = true;
 
-  void _incrementCounter() {
+  final List<Widget> _pages = [
+    const PageContent(pageTitle: 'Page 1', color: Colors.red),
+    const PageContent(pageTitle: 'Page 2', color: Colors.blue),
+    const PageContent(pageTitle: 'Page 3', color: Colors.green),
+    const PageContent(pageTitle: 'Page 4', color: Colors.orange),
+  ];
+
+  void _onItemTapped(int index) {
+    HapticFeedback.heavyImpact(); // Provide haptic feedback on item tap
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    // Add a listener to the ScrollController
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        // Scrolling down, hide the bottom nav
+        if (_isNavVisible) {
+          setState(() {
+            _isNavVisible = false;
+          });
+        }
+      } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+        // Scrolling up, show the bottom nav
+        if (!_isNavVisible) {
+          setState(() {
+            _isNavVisible = true;
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      extendBody: true, // Ensures the content extends behind the nav bar
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            controller: _scrollController,
+            child: _pages[_selectedIndex],
+          ),
+          Positioned(
+            bottom: _isNavVisible ? 0 : -80, // Position the nav bar to fall down
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+              child: Container(
+                height: 48,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 97, 97, 97).withOpacity(0.4), // Semi-transparent background
+                  borderRadius: BorderRadius.circular(16), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 37, 35, 39).withOpacity(0.2),
+                      blurRadius: 16,
+                      offset: const Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BottomNavigationBar(
+                    selectedLabelStyle: const TextStyle(fontSize: 0),
+                    unselectedLabelStyle: const TextStyle(fontSize: 0),
+                    showSelectedLabels: false,
+                    showUnselectedLabels: false,
+                    backgroundColor: Colors.transparent, // Transparent background
+                    elevation: 0, // Remove shadow
+                    type: BottomNavigationBarType.fixed,
+                    selectedItemColor: Colors.white,
+                    unselectedItemColor: const Color.fromARGB(255, 185, 185, 185),
+                    currentIndex: _selectedIndex,
+                    onTap: _onItemTapped,
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.search),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.grid_view),
+                        label: '',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.person),
+                        label: '',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PageContent extends StatelessWidget {
+  final String pageTitle;
+  final Color color;
+
+  const PageContent({super.key, required this.pageTitle, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            pageTitle,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(), // Prevent nested scrolling
+            itemCount: 30,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.circle, color: Colors.white),
+                title: Text('Item #$index', style: const TextStyle(color: Colors.white)),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
