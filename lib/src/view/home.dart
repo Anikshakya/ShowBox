1a988fab54d436e9a446c4d2c3540142bed07b0e
Anik Shakya <aniklinkin@gmail.com>
2024-12-02 18:34:20 +0545

    [Added:: shimmers in home]

--- lib/src/view/home.dart
+++ lib/src/view/home.dart
@@ -6,17 +6,20 @@ import 'package:showbox/src/view/movie/movie_details.dart';
 import 'package:showbox/src/view/series/series_details.dart';
 import 'package:showbox/src/widgets/cards/item_card.dart';
 import 'package:showbox/src/widgets/custom_image_slider.dart';
+import 'package:showbox/src/widgets/custom_shimmer.dart';
 
 class HomePage extends StatelessWidget {
   final ScrollController scrollController;
 
+  // Constructor to accept scroll controller from outside
   const HomePage({super.key, required this.scrollController});
 
   @override
   Widget build(BuildContext context) {
+    // Initialize HomeController once, outside of build to avoid reinitialization
     final homeCon = Get.put(HomeController());
 
-    // Initialize the controller
+    // Ensure the controller is initialized
     homeCon.initialize();
 
     return Scaffold(
@@ -25,131 +28,229 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 100),
+            const SizedBox(height: 100), // Spacer for the app bar
+
             // Trending Movies/Series
-            Obx(() => homeCon.isTrendingListLoading.value
-                ? const SizedBox(
-                    height: 280,
-                    child: Center(child: CircularProgressIndicator())
-                  )
-                : homeCon.trendingList.isEmpty
-                    ? const SizedBox(height: 280, child: Center(child: Text('No Trending Data Available')))
-                    : CustomImageSlider(
-                        height: 280,
-                        cornerRadius: 2.0,
-                        showIndicator: false,
-                        onTap: (index) {
-                          if (homeCon.trendingList[index]["media_type"] == "movie") {
-                            Get.to(() => MovieDetailsPage(movieId: homeCon.trendingList[index]["id"]));
-                          }
-                          if (homeCon.trendingList[index]["media_type"] == "tv") {
-                            Get.to(() => SeriesDetailPage(id: homeCon.trendingList[index]["id"]));
-                          }
-                        },
-                        images: homeCon.trendingList.map((item) {
-                          return item['poster_path'] != null
-                              ? '${AppConstants.imageUrl}${item['poster_path']}'
-                              : '';
-                        }).toList(),
-                      ),
-            ),
+            buildTrendingSection(homeCon),
+
             const SizedBox(height: 20),
-            // UpComming Movies
-            SectionWidget(
-              title: "Upcomming Movies",
-              loading: homeCon.isUpcommingMoviesLoding.value,
-              dataList: homeCon.upcommingMovies,
-              itemBuilder: (data) => ItemCard(
-                width: 132,
-                title: data["title"],
-                year: "",
-                rating: data["vote_average"]?.toDouble() ?? 0.0,
-                image: '${AppConstants.imageUrl}${data["poster_path"]}',
-              ),
-            ),
+
+            // Upcoming Movies
+            buildUpcomingMoviesSection(homeCon),
+
             const SizedBox(height: 30),
+
             // Top Rated Series Section
-            SectionWidget(
-              title: "Top Rated Series",
-              loading: homeCon.isTopRatedSeriesLoading.value,
-              dataList: homeCon.topRatedSeries,
-              itemBuilder: (movie) => ItemCard(
-                width: 132,
-                title: movie.name,
-                year: "",
-                rating: movie.voteAverage.toDouble(),
-                image: '${AppConstants.imageUrl}${movie.posterPath}',
-              ),
-            ),
+            buildTopRatedSeriesSection(homeCon),
+
             const SizedBox(height: 30),
-            // Top Rated Movies
-            SectionWidget(
-              title: "Top Rated Movies",
-              loading: homeCon.isTopRatedMoviesLoading.value,
-              dataList: homeCon.topRatedMovies,
-              itemBuilder: (movie) => ItemCard(
-                width: 132, 
-                title: movie.title,
-                year: "",
-                rating: movie.voteAverage.toDouble(),
-                image: '${AppConstants.imageUrl}${movie.posterPath}',
-              ),
-            ),
-            const SizedBox(height: 40),
+
+            // Top Rated Movies Section
+            buildTopRatedMoviesSection(homeCon),
+
+            const SizedBox(height: 40), // Extra spacer
           ],
         ),
       ),
     );
   }
-}
 
-class SectionWidget<T> extends StatelessWidget {
-  final String title;
-  final bool loading;
-  final List<T> dataList;
-  final Widget Function(T data) itemBuilder;
-
-  const SectionWidget({
-    super.key,
-    required this.title,
-    required this.loading,
-    required this.dataList,
-    required this.itemBuilder,
-  });
-
-  @override
-  Widget build(BuildContext context) {
+  // Method to build the trending section with conditional loading
+  Widget buildTrendingSection(HomeController homeCon) {
+    return Obx(
+      () => homeCon.isTrendingListLoading.isTrue
+          ? AppShimmers().trendingMovieSeriesShimmer()
+          : homeCon.trendingList.isEmpty
+              ? const SizedBox(
+                  height: 280,
+                  child: Center(child: Text("No data available")),
+                )
+              : CustomImageSlider(
+                  height: 280,
+                  cornerRadius: 2.0,
+                  showIndicator: false,
+                  onTap: (index) {
+                    final mediaType = homeCon.trendingList[index]["media_type"];
+                    final id = homeCon.trendingList[index]["id"];
+                    // Navigate based on media type (movie or TV)
+                    if (mediaType == "movie") {
+                      Get.to(() => MovieDetailsPage(movieId: id));
+                    } else if (mediaType == "tv") {
+                      Get.to(() => SeriesDetailPage(id: id));
+                    }
+                  },
+                  images: homeCon.trendingList.map((item) {
+                    return item['poster_path'] != null
+                        ? '${AppConstants.imageUrl}${item['poster_path']}'
+                        : '';
+                  }).toList(),
+                ),
+    );
+  }
+  // Method to build Upcoming Movies section with conditional loading
+  Widget buildUpcomingMoviesSection(HomeController homeCon) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 10),
+        const Padding(
+          padding: EdgeInsets.only(left: 10),
           child: Text(
-            title,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            "Upcoming Movies",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
         ),
         const SizedBox(height: 12),
-        loading
-            ? const SizedBox(height: 180, child: Center(child: CircularProgressIndicator()))
-            : dataList.isEmpty
-                ? const SizedBox(height: 180, child: Center(child: Text('No Data Available')))
-                : SizedBox(
-                    height: 180,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      itemCount: dataList.length,
-                      physics: const BouncingScrollPhysics(),
-                      separatorBuilder: (context, index) => const SizedBox(width: 10),
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {},
-                          child: itemBuilder(dataList[index]),
-                        );
-                      },
+        Obx(
+          () => homeCon.isUpcommingMoviesLoding.isTrue
+              ? SizedBox(
+                  height: 180,
+                  child: AppShimmers().buildMovieSeriesShimmerList(),
+                )
+              : homeCon.upcommingMovies.isEmpty
+                  ? const SizedBox(
+                      height: 180,
+                      child: Center(child: Text("No data available")),
+                    )
+                  : SizedBox(
+                      height: 180,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 10),
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        itemCount: homeCon.upcommingMovies.length,
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final data = homeCon.upcommingMovies[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Get.to(() => MovieDetailsPage(movieId: data["id"]));
+                            },
+                            child: ItemCard(
+                              width: 132,
+                              title: data["title"],
+                              year: "", // You can customize to show release year if needed
+                              rating: data["vote_average"]?.toDouble() ?? 0.0,
+                              image:
+                                  '${AppConstants.imageUrl}${data["poster_path"]}',
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                  ),
+        ),
+      ],
+    );
+  }
+
+  // Method to build Top Rated Series section with conditional loading
+  Widget buildTopRatedSeriesSection(HomeController homeCon) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            "Top Rated Series",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Obx(
+          () => homeCon.isTopRatedSeriesLoading.isTrue
+              ? SizedBox(
+                  height: 180,
+                  child: AppShimmers().buildMovieSeriesShimmerList(),
+                )
+              : homeCon.topRatedSeries.isEmpty
+                  ? const SizedBox(
+                      height: 180,
+                      child: Center(child: Text("No data available")),
+                    )
+                  : SizedBox(
+                      height: 180,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 10),
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        itemCount: homeCon.topRatedSeries.length,
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final movie = homeCon.topRatedSeries[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Get.to(() => SeriesDetailPage(id: movie.id));
+                            },
+                            child: ItemCard(
+                              width: 132,
+                              title: movie.name,
+                              year: "", // You can customize to show release year if needed
+                              rating: movie.voteAverage.toDouble(),
+                              image:
+                                  '${AppConstants.imageUrl}${movie.posterPath}',
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+        ),
+      ],
+    );
+  }
+
+  // Method to build Top Rated Movies section with conditional loading
+  Widget buildTopRatedMoviesSection(HomeController homeCon) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            "Top Rated Movies",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Obx(
+          () => homeCon.isTopRatedMoviesLoading.isTrue
+              ? SizedBox(
+                  height: 180,
+                  child: AppShimmers().buildMovieSeriesShimmerList(),
+                )
+              : homeCon.topRatedMovies.isEmpty
+                  ? const SizedBox(
+                      height: 180,
+                      child: Center(child: Text("No data available")),
+                    )
+                  : SizedBox(
+                      height: 180,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 10),
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        itemCount: homeCon.topRatedMovies.length,
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final movie = homeCon.topRatedMovies[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Get.to(() => MovieDetailsPage(movieId: movie.id));
+                            },
+                            child: ItemCard(
+                              width: 132,
+                              title: movie.title,
+                              year: "", // You can customize to show release year if needed
+                              rating: movie.voteAverage.toDouble(),
+                              image:
+                                  '${AppConstants.imageUrl}${movie.posterPath}',
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+        ),
       ],
     );
   }
