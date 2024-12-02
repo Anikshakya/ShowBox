c467a2d6fd255d9212864b1ad8dbe29e08d89190
Anik Shakya <aniklinkin@gmail.com>
2024-12-02 17:21:14 +0545

    [Modified:: home page] - added null checks

--- lib/src/controller/home_controller.dart
+++ lib/src/controller/home_controller.dart
@@ -1,127 +1,117 @@
-  import 'package:flutter/foundation.dart';
-import 'package:get/get.dart' hide FormData, MultipartFile;
-  import 'package:showbox/src/app_config/api_repo.dart';
-  import 'package:showbox/src/constant/constants.dart';
-  import 'package:showbox/src/models/top_rated_movies_model.dart';
-  import 'package:showbox/src/models/top_rated_series_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:showbox/src/app_config/api_repo.dart';
+import 'package:showbox/src/constant/constants.dart';
+import 'package:showbox/src/models/top_rated_movies_model.dart';
+import 'package:showbox/src/models/top_rated_series_model.dart';
 
-  class HomeController extends GetxController{
-    late RxBool isTrendingListLoading = true.obs;
-    late RxBool isTopRatedMoviesLoading = true.obs;
-    late RxBool isTopRatedSeriesLoading = true.obs;
-    late RxBool isUpcommingMoviesLoding = true.obs;
+class HomeController extends GetxController {
+  late RxBool isTrendingListLoading = true.obs;
+  late RxBool isTopRatedMoviesLoading = true.obs;
+  late RxBool isTopRatedSeriesLoading = true.obs;
+  late RxBool isUpcommingMoviesLoding = true.obs;
 
-    // Trending List
-    var trendingList = [].obs;
+  // Trending List
+  var trendingList = <dynamic>[].obs;
 
-    // Top Rated Movies
-    var topRatedMovies = [].obs;
+  // Top Rated Movies
+  var topRatedMovies = <TopRatedMoviesModel>[].obs;
 
-    // Top Rated Series
-    var topRatedSeries = [].obs;
+  // Top Rated Series
+  var topRatedSeries = <TopRatedSeriesModel>[].obs;
 
-    // UpComming Movies
-    var upcommingMovies = [].obs;
+  // UpComing Movies
+  var upcommingMovies = <dynamic>[].obs;
 
-    // Initialize data
-    void initialize() async {
-      if (trendingList.isEmpty) {
-        await getTrendingList();
-      }
-      if (upcommingMovies.isEmpty) {
-        await getUpcommingMovies();
-      }
-      if (topRatedMovies.isEmpty) {
-        await getTopRatedMovies();
-      }
-      if (topRatedSeries.isEmpty) {
-        await getTopRatedSeries();
-      }
+  // Initialize data
+  void initialize() async {
+    if (trendingList.isEmpty) {
+      await getTrendingList();
     }
-
-    // Get Trend List
-    getTrendingList() async {
-      try {
-        isTrendingListLoading(true);
-        var response = await ApiRepo.apiGet(AppConstants.trendingUrl, "", "Get Trending List");
-        if(response != null) {
-          trendingList.value = response['results'];
-          isTrendingListLoading(false);
-        }
-      } catch (e) {
-        isTrendingListLoading(false);
-      } finally {
-        isTrendingListLoading(false);
-      }
+    if (upcommingMovies.isEmpty) {
+      await getUpcommingMovies();
     }
-
-    // Get Upcomming Movies from API
-    getUpcommingMovies() async {
-      try {
-        isUpcommingMoviesLoding(true);
-
-        // Fetch data from the API
-        var response = await ApiRepo.apiGet(
-          'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
-          "",
-          "Get Upcomming Movies"
-        );
-        if (response != null) {
-          upcommingMovies.value = response['results'];
-          isUpcommingMoviesLoding(false);
-        }
-      } catch (e) {
-        isUpcommingMoviesLoding(false);
-        debugPrint('Error fetching top rated series: $e');
-      } finally {
-        isUpcommingMoviesLoding(false);
-      }
+    if (topRatedMovies.isEmpty) {
+      await getTopRatedMovies();
     }
-
-    // Get Top Rated Movies
-    getTopRatedMovies() async {
-      try {
-        isTopRatedMoviesLoading(true);
-        var response = await ApiRepo.apiGet(
-          'https://api.themoviedb.org/3/movie/top_rated', 
-          '', 
-          'Get Top Rated Movies'
-        );
-        if (response != null) {
-          topRatedMovies.value = (response['results'] as List)
-              .map((item) => TopRatedMoviesModel.fromJson(item))
-              .toList();
-          isTopRatedMoviesLoading(false);
-        }
-      } catch (e) {
-        debugPrint('Error fetching top rated movies: $e');
-      } finally {
-        isTopRatedMoviesLoading(false);
-      }
-    }
-
-    // Get Top Rated Series from API
-    getTopRatedSeries() async {
-      try {
-        isTopRatedSeriesLoading(true);
-
-        // Fetch data from the API
-        var response = await ApiRepo.apiGet(
-          'https://api.themoviedb.org/3/tv/top_rated',
-          "",
-          "Get Top Rated Series"
-        );
-        if (response != null) {
-          topRatedSeries.value = (response['results'] as List)
-            .map((item) => TopRatedSeriesModel.fromJson(item))
-            .toList();
-          isTopRatedSeriesLoading(false);
-        }
-      } catch (e) {
-        isTopRatedSeriesLoading(false);
-        debugPrint('Error fetching top rated series: $e');
-      } finally {
-        isTopRatedSeriesLoading(false);
-      }
+    if (topRatedSeries.isEmpty) {
+      await getTopRatedSeries();
     }
   }
+
+  // Get Trend List
+  getTrendingList() async {
+    try {
+      isTrendingListLoading(true);
+      var response = await ApiRepo.apiGet(AppConstants.trendingUrl, "", "Get Trending List");
+      if (response != null) {
+        trendingList.value = response['results'] ?? [];
+      }
+    } catch (e) {
+      debugPrint('Error fetching trending list: $e');
+    } finally {
+      isTrendingListLoading(false);
+    }
+  }
+
+  // Get Upcomming Movies from API
+  getUpcommingMovies() async {
+    try {
+      isUpcommingMoviesLoding(true);
+      var response = await ApiRepo.apiGet(
+        'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
+        "",
+        "Get Upcomming Movies"
+      );
+      if (response != null) {
+        upcommingMovies.value = response['results'] ?? [];
+      }
+    } catch (e) {
+      debugPrint('Error fetching upcoming movies: $e');
+    } finally {
+      isUpcommingMoviesLoding(false);
+    }
+  }
+
+  // Get Top Rated Movies
+  getTopRatedMovies() async {
+    try {
+      isTopRatedMoviesLoading(true);
+      var response = await ApiRepo.apiGet(
+        'https://api.themoviedb.org/3/movie/top_rated', 
+        '', 
+        'Get Top Rated Movies'
+      );
+      if (response != null) {
+        topRatedMovies.value = (response['results'] as List?)
+            ?.map((item) => TopRatedMoviesModel.fromJson(item))
+            .toList() ?? [];
+      }
+    } catch (e) {
+      debugPrint('Error fetching top rated movies: $e');
+    } finally {
+      isTopRatedMoviesLoading(false);
+    }
+  }
+
+  // Get Top Rated Series from API
+  getTopRatedSeries() async {
+    try {
+      isTopRatedSeriesLoading(true);
+      var response = await ApiRepo.apiGet(
+        'https://api.themoviedb.org/3/tv/top_rated',
+        "",
+        "Get Top Rated Series"
+      );
+      if (response != null) {
+        topRatedSeries.value = (response['results'] as List?)
+            ?.map((item) => TopRatedSeriesModel.fromJson(item))
+            .toList() ?? [];
+      }
+    } catch (e) {
+      debugPrint('Error fetching top rated series: $e');
+    } finally {
+      isTopRatedSeriesLoading(false);
+    }
+  }
+}
--- lib/src/view/home.dart
+++ lib/src/view/home.dart
@@ -1,226 +1,156 @@
-  import 'package:flutter/material.dart';
-  import 'package:get/get.dart';
-  import 'package:showbox/src/constant/constants.dart';
-  import 'package:showbox/src/controller/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:showbox/src/constant/constants.dart';
+import 'package:showbox/src/controller/home_controller.dart';
 import 'package:showbox/src/view/movie/movie_details.dart';
 import 'package:showbox/src/view/series/series_details.dart';
 import 'package:showbox/src/widgets/cards/item_card.dart';
-  import 'package:showbox/src/widgets/custom_image_slider.dart';
+import 'package:showbox/src/widgets/custom_image_slider.dart';
 
-  class HomePage extends StatelessWidget {
-    final ScrollController scrollController;
+class HomePage extends StatelessWidget {
+  final ScrollController scrollController;
 
-    const HomePage({super.key, required this.scrollController});
+  const HomePage({super.key, required this.scrollController});
 
-    @override
-    Widget build(BuildContext context) {
-      final homeCon = Get.put(HomeController());
+  @override
+  Widget build(BuildContext context) {
+    final homeCon = Get.put(HomeController());
 
-      // Initialize the controller
-      homeCon.initialize();
+    // Initialize the controller
+    homeCon.initialize();
 
-      return Scaffold(
-        body: SingleChildScrollView(
-          controller: scrollController,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // For App Bar
-              const SizedBox(height: 100),
-              // Trending Movies/Series
-              Obx(() => homeCon.isTrendingListLoading.isTrue
-                  ? const SizedBox(
-                      height: 280,
-                      child: Center(
-                        child: CircularProgressIndicator()
-                      )
-                    )
-                  : CustomImageSlider(
-                      height: 280,
-                      cornerRadius: 2.0,
-                      showIndicator: false,
-                      onTap: (index) {
-                        if( homeCon.trendingList[index]["media_type"] == "movie"){
-                          Get.to(()=> MovieDetailsPage(movieId: homeCon.trendingList[index]["id"]));
-                        }
-                                 
-                        if( homeCon.trendingList[index]["media_type"] == "tv"){
-                          Get.to(()=> SeriesDetailPage(
-                            id:  homeCon.trendingList[index]["id"],
-                          ));
-                        }
-                      },
-                      images: homeCon.trendingList.map((item) {
-                        return item['poster_path'] != null
-                            ? '${AppConstants.imageUrl}${item['poster_path']}'
-                            : '';
-                      }).toList(),
-                    ),
-              ),
-              const SizedBox(height: 20),
-              // UpComming Movies
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Upcomming Movies",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Obx(() => homeCon.isUpcommingMoviesLoding.isTrue
-                    ? const SizedBox(
-                      height: 180,
-                      child: Center(
-                        child: CircularProgressIndicator()
-                      )
-                    )
-                    : SizedBox(
-                        height: 180,
-                        child: ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        itemCount: homeCon.upcommingMovies.length,
-                        physics: const BouncingScrollPhysics(),
-                        separatorBuilder:(context, index) => const SizedBox(width: 10,),
-                        itemBuilder: (context, index) {
-                          final data = homeCon.upcommingMovies[index];
-                          return GestureDetector(
-                            onTap: (){
-                              Get.to(()=> MovieDetailsPage(
-                                movieId: data["id"],
-                              ));
-                            },
-                            child: ItemCard(
-                              width: 132,
-                              title: data["title"], 
-                              year: "", 
-                              rating: data["vote_average"]?.toDouble() ?? 0.0, 
-                              image: '${AppConstants.imageUrl}${data["poster_path"]}'
-                            )
-                          );
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: scrollController,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 100),
+            // Trending Movies/Series
+            Obx(() => homeCon.isTrendingListLoading.value
+                ? const SizedBox(
+                    height: 280,
+                    child: Center(child: CircularProgressIndicator())
+                  )
+                : homeCon.trendingList.isEmpty
+                    ? const SizedBox(height: 280, child: Center(child: Text('No Trending Data Available')))
+                    : CustomImageSlider(
+                        height: 280,
+                        cornerRadius: 2.0,
+                        showIndicator: false,
+                        onTap: (index) {
+                          if (homeCon.trendingList[index]["media_type"] == "movie") {
+                            Get.to(() => MovieDetailsPage(movieId: homeCon.trendingList[index]["id"]));
+                          }
+                          if (homeCon.trendingList[index]["media_type"] == "tv") {
+                            Get.to(() => SeriesDetailPage(id: homeCon.trendingList[index]["id"]));
+                          }
                         },
+                        images: homeCon.trendingList.map((item) {
+                          return item['poster_path'] != null
+                              ? '${AppConstants.imageUrl}${item['poster_path']}'
+                              : '';
+                        }).toList(),
                       ),
-                    ),
-                  ),
-                ],
+            ),
+            const SizedBox(height: 20),
+            // UpComming Movies
+            SectionWidget(
+              title: "Upcomming Movies",
+              loading: homeCon.isUpcommingMoviesLoding.value,
+              dataList: homeCon.upcommingMovies,
+              itemBuilder: (data) => ItemCard(
+                width: 132,
+                title: data["title"],
+                year: "",
+                rating: data["vote_average"]?.toDouble() ?? 0.0,
+                image: '${AppConstants.imageUrl}${data["poster_path"]}',
               ),
-              const SizedBox(height: 30),
-              // Top Rated Series Section
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Top Rated Series",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Obx(() => homeCon.isTopRatedSeriesLoading.isTrue
-                      ? const SizedBox(
-                          height: 180,
-                          child: Center(
-                            child: CircularProgressIndicator()
-                          )
-                        )
-                      : SizedBox(
-                        height: 180,
-                        child: ListView.separated(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: homeCon.topRatedSeries.length,
-                        physics: const BouncingScrollPhysics(),
-                        separatorBuilder:(context, index) => const SizedBox(width: 10,),
-                        itemBuilder: (context, index) {
-                          final movie = homeCon.topRatedSeries[index];
-                          return GestureDetector(
-                            onTap: (){
-                              Get.to(()=> SeriesDetailPage(
-                                id: movie.id,
-                              ));
-                            },
-                            child: ItemCard(
-                              width: 132,
-                              title: movie.name, 
-                              year: "", 
-                              rating: movie.voteAverage?.toDouble() ?? 0.0, 
-                              image: '${AppConstants.imageUrl}${movie.posterPath}'
-                            )
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
+            ),
+            const SizedBox(height: 30),
+            // Top Rated Series Section
+            SectionWidget(
+              title: "Top Rated Series",
+              loading: homeCon.isTopRatedSeriesLoading.value,
+              dataList: homeCon.topRatedSeries,
+              itemBuilder: (movie) => ItemCard(
+                width: 132,
+                title: movie.name,
+                year: "",
+                rating: movie.voteAverage.toDouble(),
+                image: '${AppConstants.imageUrl}${movie.posterPath}',
               ),
-              const SizedBox(height: 30),
-              // Top Rated Movies
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Top Rated Movies",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Obx(() => homeCon.isTopRatedMoviesLoading.isTrue
-                    ? const SizedBox(
-                      height: 180,
-                        child: Center(
-                          child: CircularProgressIndicator()
-                        )
-                      )
-                    : SizedBox(
-                        height: 180,
-                        child: ListView.separated(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: homeCon.topRatedMovies.length,
-                        physics: const BouncingScrollPhysics(),
-                        separatorBuilder:(context, index) => const SizedBox(width: 10,),
-                        itemBuilder: (context, index) {
-                          final movie = homeCon.topRatedMovies[index];
-                          return GestureDetector(
-                            onTap: (){
-                              Get.to(()=> MovieDetailsPage(
-                                movieId: movie.id,
-                              ));
-                            },
-                            child: ItemCard(
-                              width: 132,
-                              title: movie.title, 
-                              year: "", 
-                              rating: movie.voteAverage?.toDouble() ?? 0.0, 
-                              image: '${AppConstants.imageUrl}${movie.posterPath}'
-                            )
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
+            ),
+            const SizedBox(height: 30),
+            // Top Rated Movies
+            SectionWidget(
+              title: "Top Rated Movies",
+              loading: homeCon.isTopRatedMoviesLoading.value,
+              dataList: homeCon.topRatedMovies,
+              itemBuilder: (movie) => ItemCard(
+                width: 132, 
+                title: movie.title,
+                year: "",
+                rating: movie.voteAverage.toDouble(),
+                image: '${AppConstants.imageUrl}${movie.posterPath}',
               ),
-              const SizedBox(height: 40),
-            ],
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SectionWidget<T> extends StatelessWidget {
+  final String title;
+  final bool loading;
+  final List<T> dataList;
+  final Widget Function(T data) itemBuilder;
+
+  const SectionWidget({
+    super.key,
+    required this.title,
+    required this.loading,
+    required this.dataList,
+    required this.itemBuilder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
         ),
-      );
-    }
+        const SizedBox(height: 12),
+        loading
+            ? const SizedBox(height: 180, child: Center(child: CircularProgressIndicator()))
+            : dataList.isEmpty
+                ? const SizedBox(height: 180, child: Center(child: Text('No Data Available')))
+                : SizedBox(
+                    height: 180,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      itemCount: dataList.length,
+                      physics: const BouncingScrollPhysics(),
+                      separatorBuilder: (context, index) => const SizedBox(width: 10),
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: itemBuilder(dataList[index]),
+                        );
+                      },
+                    ),
+                  ),
+      ],
+    );
   }
+}
